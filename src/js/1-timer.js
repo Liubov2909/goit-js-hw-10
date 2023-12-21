43f9400fb8c8d79f95f993b4637a5e1d56c089f8
Liubov2909 <ostapcuk2909@gmail.com>
2023-12-21 19:05:16 +0200

    part dz 10

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,4 +1,15 @@
-// Описаний в документації
 import flatpickr from "flatpickr";
-// Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+let targetDate;
+const inputEl = document.querySelector('#datetime-picker');
+const buttonEl = document.querySelector('[data-start]');
+const timerEls = {
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
